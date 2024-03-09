2cbe1d8505aad1d687839e89023b9e83a164ac2c
srujankothuri <srujan019@gmail.com>
2024-03-09 13:52:22 +0530

    chg

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "changes made in hello.cpp"
+  cout << "changes made in hello.cpp";
   return 0;
 }
